c4b2225ca7ace49c94f0a0fc09f51fec319c67b7
Samira Champlin <smchamplin16@blakeschool.org>
2016-02-15 15:46:46 -0600

    Added ray cast script

---
+++ FarmApocalypse/Assets/Scripts/Raycast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycast : MonoBehaviour {
+	public Camera CameraFacing;
+	public float Speed;
+	public GameObject Cube;
+	void Update () {
+		RaycastHit hit;	
+		{
+			if (Physics.Raycast  (new Ray (CameraFacing.transform.position,
+				CameraFacing.transform.rotation * Vector3.forward),
+				out hit)) {
+				if (hit.collider.tag == "Test")
+				{
+					GetComponent<Rigidbody>().velocity = transform.forward * Speed;
+					Destroy (Cube);
+				}
+
+			}
+		}
+	}
+}
