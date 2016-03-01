f3cd50329d433df0d1986cded20d3e6ebce617fe
Parker Haselhorst <pjhaselhors17@blakeschool.org>
2016-03-01 13:03:35 -0600

    Created Level Transfer Script

---
+++ FarmApocalypse/Assets/Scripts/GuiLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiLevel : MonoBehaviour {
+
+		void OnGUI ()
+		{
+			if(GUI.Button(new Rect(10,10,150,100), "Click"))
+			{
+				Debug.Log("Button Clicked");
+			}
+		}
+	}
