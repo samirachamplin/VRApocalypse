da4ee340307ac93571dadd58c1d7eaba988d827a
Parker Haselhorst <pjhaselhors17@blakeschool.org>
2016-03-01 13:50:57 -0600

    Bat Script
    Needs work

---
+++ FarmApocalypse/Assets/Scripts/BatAttack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatAttack : MonoBehaviour {
+
+
+	//void onclick () {
+		//Play.animation()
+		//}
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Sheep")
+		{
+			//Apply force To sheep 
+
+		}
+	}
+}
