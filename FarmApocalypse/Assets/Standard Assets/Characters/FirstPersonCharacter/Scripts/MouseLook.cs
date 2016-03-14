232bb82e078ef549deed83bfacd455738af8dc64
Parker Haselhorst <pjhaselhors17@blakeschool.org>
2016-03-14 14:55:59 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	FarmApocalypse/Library/CurrentLayout.dwlt
    #	FarmApocalypse/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll
    #	FarmApocalypse/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb
    #	FarmApocalypse/Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll
    #
    FarmApocalypse/Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll.md
    b
    #	FarmApocalypse/Library/metadata/00/00000000000000002000000000000000

--- FarmApocalypse/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ FarmApocalypse/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
-
+using Rewired;
 namespace UnityStandardAssets.Characters.FirstPerson
 {
     [Serializable]
@@ -30,8 +30,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         public void LookRotation(Transform character, Transform camera)
         {
-            float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
-            float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+			float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+			float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
 
             m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
             m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
