c6205335f095e32cc7dcaa21fa50674b90d8ee0f
Parker Haselhorst <pjhaselhors17@TBSLTMBA871.local>
2016-04-29 14:08:47 -0500

    Merge remote-tracking branch 'origin/master-update' into master-update
    # Conflicts:
    #	FarmApocalypseRedo/Library/CurrentLayout.dwlt
    #	FarmApocalypseRedo/Library/InspectorExpandedItems.asset
    #	FarmApocalypseRedo/Library/assetDatabase3
    #	FarmApocalypseRedo/Library/expandedItems
    #
    FarmApocalypseRedo/Library/metadata/00/00000000000000004000000000000000
    #
    FarmApocalypseRedo/Library/metadata/00/00000000000000006100000000000000
    #
    FarmApocalypseRedo/Library/metadata/bd/bd5ea6de13c4343849b11a40a1b96dba
    #
    FarmApocalypseRedo/Library/metadata/d0/d0dd02f961cf44b9199d49136078858b.
    info
    #
    FarmApocalypseRedo/Library/metadata/da/daf3687436ed44ad7b63ccefdee06484.
    info
    #
    FarmApocalypseRedo/Library/metadata/dd/dddd43ee2cb004578b41bafad9629b16.
    info

---
+++ FarmApocalypseRedo/Assets/script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script : MonoBehaviour {
+
+	public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
