4b1fe15e6b85506c98e301dddfc564d73bb16c54
Parker Haselhorst <pjhaselhors17@TBSLTMBA871.local>
2016-05-03 13:52:24 -0500

    Walking Sheep

---
+++ FarmApocalypseRedo/Assets/Scripts/NewWander.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+ var Speed= 20;
+ var wayPoint : Vector3;
+ var Range= 10;
+ 
+ function Start(){
+    //initialise the target way point
+    Wander();
+ }
+ 
+ function Update() 
+ {
+    // this is called every frame
+    // do move code here
+    transform.position += transform.TransformDirection(Vector3.forward)*Speed*Time.deltaTime;
+     if((transform.position - wayPoint).magnitude < 3)
+     {
+         // when the distance between us and the target is less than 3
+         // create a new way point target
+         Wander();
+ 
+ 
+     }
+ }
+ 
+ function Wander()
+ { 
+    // does nothing except pick a new destination to go to
+     
+     wayPoint=  Vector3(Random.Range(transform.position.x - Range, transform.position.x + Range), 1, Random.Range(transform.position.z - Range, transform.position.z + Range));
+     wayPoint.y = 1;
+    // don't need to change direction every frame seeing as you walk in a straight line only
+     transform.LookAt(wayPoint);
+     Debug.Log(wayPoint + " and " + (transform.position - wayPoint).magnitude);
+ }
